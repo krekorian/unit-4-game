5d5a0deb4ac85ee9929269653da79f08d94f0663
krekorian@gmail.com <krekorian@gmail.com>
2019-04-04 21:01:15 -0700

    brach_1_04042019_5

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,69 @@
 $(document).ready(function () {
+    /*var crystals = { diamond: 0, emerald: 0, yellow: 0, ruby: 0 }
+    crystals.diamond = Math.floor(Math.random() * 12) + 1;
+    crystals.emerald = Math.floor(Math.random() * 12) + 1;
+    crystals.yellow = Math.floor(Math.random() * 12) + 1;
+    crystals.ruby = Math.floor(Math.random() * 12) + 1;
+*/
 
+    crystals = {}
+    var out_number;
+    var noToGuess;
+    var winCounter = 0;
+    var loseCounter = 0;
+    $(function startUp() {
+        console.log("Vartan");
+        $("#guess-number").text(0);
+        out_number = 0;
+        crystals = { diamond: 0, emerald: 0, yellow: 0, ruby: 0 }
+        crystals.diamond = Math.floor(Math.random() * 12) + 1;
+        crystals.emerald = Math.floor(Math.random() * 12) + 1;
+        crystals.yellow = Math.floor(Math.random() * 12) + 1;
+        crystals.ruby = Math.floor(Math.random() * 12) + 1;
+        noToGuess = Math.floor(Math.random() * 101) + 19;
+        $("#toGuessNumber").text(noToGuess);
+        $("#win-count").text(winCounter);
+        $("#loss-count").text(loseCounter);
+    });
+    function startOver() {
+        out_number = 0;
+        crystals = { diamond: 0, emerald: 0, yellow: 0, ruby: 0 }
+        crystals.diamond = Math.floor(Math.random() * 12) + 1;
+        crystals.emerald = Math.floor(Math.random() * 12) + 1;
+        crystals.yellow = Math.floor(Math.random() * 12) + 1;
+        crystals.ruby = Math.floor(Math.random() * 12) + 1;
+        noToGuess = Math.floor(Math.random() * 101) + 19;
+        $("#toGuessNumber").text(noToGuess);
+        $("#win-count").text(winCounter);
+        $("#loss-count").text(loseCounter);
+    }
+    /*(function ($) {
+        $.fn.myfunction = function () {
+            console.log("Vartan");
+            return this;
+        };
+    })(jQuery);
+*/
     $('.crystal-square').on('click', function () {
         entered_value = ($(this).attr("value"));
         //$("#print-out").text(entered_value);
-        console.log(entered_value);
-        $("#guess-number").text(entered_value);
+        //console.log(entered_value);
+        //console.log(crystals.diamond);
+        //console.log(crystals.emerald);
+        //console.log(crystals.yellow);
+        //console.log(crystals.ruby);
+        //console.log(crystals[entered_value]);
+        out_number += crystals[entered_value]
+        $("#guess-number").text(out_number);
+        if (out_number === noToGuess) {
+            winCounter++;
+            startOver();
+        }
+        else if (out_number > noToGuess) {
+            loseCounter++;
+            startOver();
 
+        }
     });
 
 });
