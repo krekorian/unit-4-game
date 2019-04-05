d06c72b1ed0bf3be8d6e09e4d942d5f166be3351
krekorian@gmail.com <krekorian@gmail.com>
2019-04-04 18:13:09 -0700

    brach_1_04042019_4

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,11 @@
 $(document).ready(function () {
 
+    $('.crystal-square').on('click', function () {
+        entered_value = ($(this).attr("value"));
+        //$("#print-out").text(entered_value);
+        console.log(entered_value);
+        $("#guess-number").text(entered_value);
 
+    });
 
 });
